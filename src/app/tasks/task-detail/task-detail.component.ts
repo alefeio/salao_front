8959a441a2d130e1615399210b440b726c98557d
Alexandre Feio <alefeio@gmail.com>
2019-04-09 17:39:13 -0300

    switchMap e subscribe no task detail

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+
+import { switchMap } from 'rxjs/operators';
 
 import { Task } from '../shared/task.model'
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from '../shared/task.service';
 
 @Component({
     selector: 'task-detail',
     templateUrl: './task-detail.component.html', 
 })
 
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit {
     @Input() public task: Task
 
-    public constructor(){
-        this.task = new Task(1, 'Tarefa incluída manualmente. ')
+    public constructor(
+        private route: ActivatedRoute,
+        private taskService: TaskService
+    ){ }
+
+    public ngOnInit() {
+        this.route.params
+            .pipe(switchMap((params: Params) => this.taskService.getTask(+params['id'])))
+            .subscribe(task => {
+                this.task = task
+                console.log(this.task)
+            })
     }
 
 }
